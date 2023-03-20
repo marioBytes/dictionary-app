19813167464cd1dce16c960fe022eee31d2dd55b
marioBytes <mario.bytes92@gmail.com>
2023-03-20 14:14:54 -0600

    Import custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      'black': '#050505',
+      'gray': '#1f1f1f',
+      'light-gray': '#2d2d2d',
+      'lighter-gray': '#3a3a3a',
+      'even-lighter-gray': '#757575',
+      'lightest-gray': '#e9e9e9',
+      'off-white': '#f4f4f4',
+      'white': '#ffffff',
+      'purple': '#a445ed',
+      'red': '#ff5252',
+    },
   },
   plugins: [],
 };
