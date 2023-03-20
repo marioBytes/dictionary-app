dcdbc28ec942ce8a3d7cdd7cc4b2805002c235e8
marioBytes <mario.bytes92@gmail.com>
2023-03-20 14:02:55 -0600

    Create context to show/update global font

---
+++ src/contexts/FontContext.js
@@ -0,0 +1,5 @@
+import { createContext } from 'react';
+
+const FontContext = createContext(null);
+
+export default FontContext;
--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,9 +1,32 @@
+import { useState } from 'react';
+
+import Topbar from '../components/Topbar/Topbar';
+import FontContext from '../contexts/FontContext';
+
+import { getStorageValue } from '../utils/localStorageHelpers';
 import '../App.css';
 
 function Root() {
   return (
-    <div>Hello World!</div>
+    <FontProvider>
+      <Topbar />
+    </FontProvider>
   );
+}
+
+const FontProvider = ({ children }) => {
+  const storedFont = getStorageValue('font')
+  const currentFont = storedFont === null ? 'sans-serif' : storedFont;
+
+  const [font, setFont] = useState(currentFont);
+
+  return (
+    <FontContext.Provider value={{ font, setFont }}>
+      <div style={{ fontFamily: font }}>
+        {children}
+      </div>
+    </FontContext.Provider>
+  )
 }
 
 export default Root;
