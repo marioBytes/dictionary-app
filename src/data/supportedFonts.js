b8ae5ec499d24ebb40ba4cc935ddb32bbac46715
marioBytes <mario.bytes92@gmail.com>
2023-03-20 14:00:26 -0600

    Create area to hold supported fonts

---
+++ src/data/supportedFonts.js
@@ -0,0 +1,7 @@
+const supportedFonts = [
+  { id: 1, name: 'Sans Serif', cssName: 'sans-serif' },
+  { id: 2, name: 'Serif', cssName: 'serif' },
+  { id: 3, name: 'Mono', cssName: 'monospace' },
+];
+
+export default supportedFonts;
