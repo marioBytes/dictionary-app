203c572791f9a2abfaf46bca9aa28b68d5f72b50
marioBytes <mario.bytes92@gmail.com>
2023-03-21 14:41:11 -0600

    Track when theme updates

--- src/components/Topbar/theme/Switch.jsx
+++ src/components/Topbar/theme/Switch.jsx
@@ -1,7 +1,50 @@
+import { useEffect, useState } from 'react';
+
+import useThemeDetector from '../../../hooks/useThemeDetector';
+import { getStorageValue, setStorageValue } from '../../../utils/localStorageHelpers';
+
 const Switch = () => {
+  const prefersDarkTheme = useThemeDetector();
+  const [theme, setTheme] = useState(prefersDarkTheme ? 'dark' : 'light');
+
+  useEffect(() => {
+    const storedTheme = getStorageValue('theme');
+    if (!storedTheme) {
+      setStorageValue('theme', theme);
+      setTheme(theme);
+    } else {
+      setTheme(storedTheme);
+    }
+
+    updateThemeClassList(theme, theme === 'dark' ? 'light' : 'dark');
+  }, [theme]);
+
+  const switchTheme = () => {
+    const storedTheme = getStorageValue('theme');
+    let newTheme;
+    let oldTheme;
+
+    if (storedTheme === 'dark') {
+      newTheme = 'light';
+      oldTheme = 'dark';
+    } else {
+      newTheme = 'dark';
+      oldTheme = 'light';
+    }
+
+    setStorageValue('theme', newTheme);
+    setTheme(newTheme);
+    updateThemeClassList(newTheme, oldTheme);
+  };
+
+  const updateThemeClassList = (add, remove) => {
+    document.documentElement.classList.add(add);
+    document.documentElement.classList.remove(remove);
+  };
+
   return (
     <div>
-      <p>Switch</p>
+      <button onClick={switchTheme}>switch</button>
     </div>
   );
 };
