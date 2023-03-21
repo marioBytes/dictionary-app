61569dd73d42614a976a33cc1424c245857c7b4b
marioBytes <mario.bytes92@gmail.com>
2023-03-21 15:58:40 -0600

    Remove comments

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -8,15 +8,12 @@ import Theme from './theme/Theme';
 const Topbar = () => {
   return (
     <div className="flex justify-between items-center" style={{ height: '34px' }}>
-      {/* logo */}
       <Logo />
       <div className='flex h-full gap-6'>
-        {/* dropdown to change font */}
         <DropdownProvider>
           <Dropdown />
         </DropdownProvider>
         <div className="h-full bg-even-lighter-gray opacity-25" style={{ width: '2px' }}/>
-        {/* theme switcher */}
         <Theme />
       </div>
     </div>
