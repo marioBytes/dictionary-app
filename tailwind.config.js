f5d6c173673ff1205e7c64324b9a5203cfa8395b
marioBytes <mario.bytes92@gmail.com>
2024-03-19 15:20:34 -0600

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    colors: {
+      black: '#050505',
+      darkestGray: '#1f1f1f',
+      darkerGray: '#2d2d2d',
+      darkGray: '#3a3a3a',
+      gray: '#757575',
+      offWhite: '#f4f4f4',
+      white: '#ffffff',
+      purple: '#a445ed',
+      red: '#ff5252',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
