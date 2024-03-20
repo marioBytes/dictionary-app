f669af58c3f9e9025698bdf4331361df3fb93097
marioBytes <mario.bytes92@gmail.com>
2024-03-20 13:11:38 -0600

    Add theme switch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, effect, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { DataService } from './data.service';
 import { defaultWord } from './default_word';
 
+import { ThemeSwitchComponent } from './theme-switch/theme-switch.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ThemeSwitchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   providers: [HttpClient],
 })
 export class AppComponent {
+  darkMode = signal<boolean>(
+    JSON.parse(window.localStorage.getItem('darkMode') ?? 'false')
+  );
+
   data: any;
-  title = 'dictionary-app';
   keyword: string = 'keyboard';
 
-  constructor(private dataService: DataService) {}
+  constructor(private dataService: DataService) {
+    effect(() => {
+      window.localStorage.setItem('darkMode', JSON.stringify(this.darkMode()));
+    });
+  }
+
+  @HostBinding('class.dark') get mode() {
+    return this.darkMode();
+  }
 
   ngOnInit() {
     this.data = defaultWord;
